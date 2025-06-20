e7ab244472a7f16c8edf0b877b932da629ca84a7
Erik De Boeck <deboeck.erik@gmail.com>
2025-06-20 14:07:35 +0200

    Python code, README.md and empty folders
    Signed-off-by: Erik De Boeck <deboeck.erik@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # bulk-changes-to-tmx
-Simple code to make bulk changes to a .tmx translation memory
+This is a simple code to make bulk changes to a .tmx translation memory.
+
+## Requirements
+### Python virtual environment
+A "virtual environment" is recommended; I trust everyone's capabilities of finding out how that works. I don't think any dependencies need to be installed, but if some do, again: the answer is somewhere out there, on the internet.
+
+### Regex
+The detection is done based on regex substrings. Again, the internet can tell you all about it.
+
+## Input files
+Only files with the extension .txm will be handled. Obviously, the files need to be put in the directory `input`.
+
+I wrote the script based on a .tmx file made by OmegaT with a syntax similar to this:
+```
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tmx SYSTEM "tmx11.dtd">
+<tmx version="1.1">
+  <header creationtool="OmegaT" o-tmf="OmegaT TMX" adminlang="EN-US" datatype="plaintext" creationtoolversion="6.0.1_0_42ef0143" segtype="sentence" srclang="en-GB"/>
+  <body>
+<!-- Default translations -->
+    <tu>
+      <tuv lang="en-GB">
+        <seg>Hello World!</seg>
+      </tuv>
+      <tuv lang="nl-NL" changeid="Erik DB" changedate="20210117T112458Z" creationid="Erik DB" creationdate="20210117T112458Z">
+        <seg>Dag wereld!</seg>
+      </tuv>
+    </tu>
+<!-- Alternative translations -->
+    <tu>
+      <prop type="file">Some_text.docx</prop>
+      <prop type="prev">Hello World!</prop>
+      <prop type="next">Bye!</prop>
+      <tuv lang="en-GB">
+        <seg>Sun is shining! Let the vitamine C come to me!</seg>
+      </tuv>
+      <tuv lang="nl-NL" changeid="Erik DB" changedate="20220306T093912Z" creationid="Erik DB" creationdate="20220306T093912Z">
+        <seg>De zon schijnt! Laat die vitamine D maar komen!</seg>
+      </tuv>
+    </tu>
+  </body>
+</tmx>
+```
+
+If your file has a different structure, the code will most likely need to be altered.
+
+## Output files
+I wasn't able to get the two comments (`<!-- Default translations -->`) and (`<!-- Alternative translations -->`) on exactly the same spot, they're part of the `<body>` element in the output file. I don't think that matters, since they're just comments. But I mention it for full disclosure. (And if anyone has a solution for it, please let me know.)
+
+Obviously, the files will appear in the directory `output`.
+
+## Variables
+### remove_old_segments
+If you want to **replace** segments, make sure `remove_old_segments` is `True`. If it is `False`, the new segments will simply be added to the existing memory.
+
+### regex_substrings_to_change
+This list should contain more lists, where the first element is the language of the `tuv` tag, the second element the regex substring that will be searched, the third element is the substring that will replace the substring that was found (i.e. the second element).
+
+If you want to change `D` to `C` in the segments in the language `en-GB`, this is what the list should look like:
+```
+regex_substrings_to_change = [
+    ["en-GB", r"\bC\b", "D"] // Don't forget to add a comma if you have more elements!
+]
+```
---
+++ bulk-changes-to-tmx.py
@@ -0,0 +1,95 @@
+###########################################################################################################
+###########################################################################################################
+### Adjust these variables to your situation:
+
+## If you want to remove changed segments, instead of adding changed so the memory, set to "True".
+# If set to "False", the changed segments will simply be added to the existing memory.
+
+remove_old_segments = False
+
+## Remember that every line changes the string, so if you want to change "apple tree" to "strawberry bush",
+# first changing "tree" to "bush" and only then changing "apple tree" to "strawberry bush" is a bad idea.
+
+regex_substrings_to_change = [
+    ["en-GB", r"\bC\b", "D"] # Don't forget to add a comma if you have more elements!
+]
+
+###########################################################################################################
+###########################################################################################################
+
+
+import xml.etree.ElementTree as ET
+import os
+import re
+import copy
+from datetime import datetime
+
+
+def select_files():
+    os.chdir("input")
+    for file in os.listdir():
+        if file.endswith(".tmx"):
+            inspect_segments(file)
+        else:
+            print('"{}" is not a .tmx file; it will be ignored.'.format(file))
+
+
+def inspect_segments(input_file):
+    
+    tree = ET.parse(input_file)
+    body = tree.getroot()[1]
+    alternative_translations_comment_inserted = False
+    position = 0
+    for tu in body:
+        position = position + 1
+        copy_of_tu = copy.deepcopy(tu)
+        retain_copy_of_tu = False
+        for x in range (tu.__len__()):
+            if ((not alternative_translations_comment_inserted) and tu[x].tag == "prop"):
+                alternative_translations_comment = ET.Comment("Alternative translations") ## Sadly, these comments are not at the root level, but I couldn't figure out how to do that.
+                body.insert(position - 1, alternative_translations_comment)
+                alternative_translations_comment_inserted = True
+            if (tu[x].tag == "tuv"):
+                for y in range (tu[x].__len__()):
+                    if (tu[x][y].tag == "seg"):
+                        retain_copy_of_tu = bulk_change_segments(tu[x].attrib["lang"], tu[x][y].text, copy_of_tu, x, y, retain_copy_of_tu)
+
+        if retain_copy_of_tu:
+            body.append(copy_of_tu)
+        
+        if retain_copy_of_tu and remove_old_segments:
+            body.remove(tu)
+    
+    default_translations_comment = ET.Comment("Default translations") ## Sadly, these comments are not at the root level, but I couldn't figure out how to do that.
+    body.insert(0, default_translations_comment)
+
+    os.chdir("../output")
+    with open(input_file, 'wb') as f:
+        f.write('<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE tmx SYSTEM "tmx11.dtd">'.encode('utf8'))
+        tree.write(f, 'utf-8')
+    
+    print('Bulk changes in file "{}" have been done.'.format(input_file))
+    os.chdir("../input")
+
+
+def bulk_change_segments(language, segment_text, copy_of_tu, x, y, retain_copy_of_tu):
+    new_segment_text = segment_text
+    for to_check_substring in regex_substrings_to_change:
+        if (language == to_check_substring[0] and (re.search(to_check_substring[1], segment_text) != None)):
+            retain_copy_of_tu = True
+            new_segment_text = re.sub(to_check_substring[1], to_check_substring[2], new_segment_text)
+
+    if (retain_copy_of_tu):
+        copy_of_tu[x][y].text = new_segment_text
+        if (language == "nl-NL"):
+            now = datetime.now()
+            nowstring = now.strftime('%Y%m%dT%H%M%SZ')
+            copy_of_tu[x].set('creationdate', nowstring)
+            copy_of_tu[x].set('changedate', nowstring)
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    select_files()
