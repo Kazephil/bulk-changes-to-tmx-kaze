de6130ad8ec295e9aefacb54c83913d42ef2cee3
Kazephil <kazephil@gmail.com>
2025-06-22 23:13:04 +0900

    Change regex substrings to match my own TMX files, and add code to handle both  namespaced and non-namespaced attributes.

--- bulk-changes-to-tmx.py
+++ bulk-changes-to-tmx.py
@@ -24,12 +24,12 @@ change_changeid = False
 # The last element of each list is "True", if it is for a target languague.
 
 regex_substrings_to_change = [
-    ["en-GB", r"\bC\b", "D", False],
-    ["nl-NL", r"\bC\b", "D", True],
-    ["en-GB", r"strawberries", "apples", False],
-    ["nl-NL", r"aardbeien", "appels", True],
-    ["en-GB", r"strawberry", "apple", False],
-    ["nl-NL", r"aardbei", "appel", True] # Don't forget to add a comma if you have more elements!
+    ["en-CA", r"\bC\b", "D", False],
+    ["fr-CA", r"\bC\b", "D", True],
+    ["en-CA", r"tea", "juice", False],
+    ["fr-CA", r"thé", "jus", True],
+    ["en-CA", r"'", "ʼ", False],
+    ["fr-CA", r"'", "ʼ", True],  # Don't forget to add a comma if you have more elements!
 ]
 
 ###########################################################################################################
@@ -41,6 +41,8 @@ import os
 import re
 import copy
 from datetime import datetime
+
+XMLLANG = ET.QName('http://www.w3.org/XML/1998/namespace', 'lang')
 
 
 def select_files():
@@ -55,7 +57,9 @@ def select_files():
 def inspect_segments(input_file):
     tree = ET.parse(input_file)
     body = tree.getroot()[1]
-    tree.getroot().attrib["version"] = "1.4"
+    version = tree.getroot().attrib.get("version")
+    LANG = XMLLANG if version == "1.4" else "lang"
+
     alternative_translations_comment_inserted = False
     position = 0
     to_insert_copies_of_tu = []
@@ -64,38 +68,44 @@ def inspect_segments(input_file):
         copy_of_tu = copy.deepcopy(tu)
         retain_copy_of_tu = False
         this_is_an_alternative_translation = False
-        for x in range (tu.__len__()):
-            if (tu[x].tag == "prop"):
+        for x in range(tu.__len__()):
+            if tu[x].tag == "prop":
                 this_is_an_alternative_translation = True
-                if (not alternative_translations_comment_inserted):
-                    alternative_translations_comment = ET.Comment("Alternative translations") ## Sadly, these comments are not at the root level, but I couldn't figure out how to do that.
+                if not alternative_translations_comment_inserted:
+                    alternative_translations_comment = ET.Comment(
+                        "Alternative translations"
+                    )  ## Sadly, these comments are not at the root level, but I couldn't figure out how to do that.
                     body.insert(position - 1, alternative_translations_comment)
                     alternative_translations_comment_inserted = True
-            if (tu[x].tag == "tuv"):
-                for y in range (tu[x].__len__()):
-                    if (tu[x][y].tag == "seg"):
-                        retain_copy_of_tu = bulk_change_segments(tu[x].attrib["lang"], tu[x][y].text, copy_of_tu, x, y, retain_copy_of_tu)
+            if tu[x].tag == "tuv":
+                for y in range(tu[x].__len__()):
+                    if tu[x][y].tag == "seg":
+                        retain_copy_of_tu = bulk_change_segments(
+                            tu[x].attrib[LANG], tu[x][y].text, copy_of_tu, x, y, retain_copy_of_tu
+                        )
 
         if retain_copy_of_tu:
-            if (this_is_an_alternative_translation):
+            if this_is_an_alternative_translation:
                 body.append(copy_of_tu)
             else:
                 to_insert_copies_of_tu.append(copy_of_tu)
-        
+
             if remove_old_segments:
                 body.remove(tu)
-    
+
     for to_insert_copy_of_tu in to_insert_copies_of_tu:
         body.insert(0, to_insert_copy_of_tu)
-    
-    default_translations_comment = ET.Comment("Default translations") ## Sadly, these comments are not at the root level, but I couldn't figure out how to do that.
+
+    default_translations_comment = ET.Comment(
+        "Default translations"
+    )  ## Sadly, these comments are not at the root level, but I couldn't figure out how to do that.
     body.insert(0, default_translations_comment)
 
     os.chdir("../output")
     with open(input_file, 'wb') as f:
         f.write('<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE tmx SYSTEM "tmx14.dtd">'.encode('utf8'))
         tree.write(f, 'utf-8')
-    
+
     print('Bulk changes in file "{}" have been done.'.format(input_file))
     os.chdir("../input")
 
@@ -104,32 +114,32 @@ def bulk_change_segments(language, segment_text, copy_of_tu, x, y, retain_copy_o
     new_segment_text = segment_text
     changes_made_now = False
     for to_check_substring in regex_substrings_to_change:
-        if (language == to_check_substring[0] and (re.search(to_check_substring[1], segment_text) != None)):
+        if language == to_check_substring[0] and (re.search(to_check_substring[1], segment_text) != None):
             retain_copy_of_tu = True
             changes_made_now = True
             new_segment_text = re.sub(to_check_substring[1], to_check_substring[2], new_segment_text)
 
-    if (changes_made_now):
+    if changes_made_now:
         copy_of_tu[x][y].text = new_segment_text
-        if (to_check_substring[3]):
-            if (change_creationdate):
+        if to_check_substring[3]:
+            if change_creationdate:
                 now = datetime.now()
                 nowstring = now.strftime('%Y%m%dT%H%M%SZ')
                 copy_of_tu[x].set('creationdate', nowstring)
-            if (change_changedate):
+            if change_changedate:
                 now = datetime.now()
                 nowstring = now.strftime('%Y%m%dT%H%M%SZ')
                 copy_of_tu[x].set('changedate', nowstring)
-            if (change_creationid):
+            if change_creationid:
                 copy_of_tu[x].set('creationid', change_creationid)
-            if (change_changeid):
+            if change_changeid:
                 copy_of_tu[x].set('creationid', change_changeid)
 
-    if (retain_copy_of_tu):        
+    if retain_copy_of_tu:
         return True
     else:
         return False
 
-    
+
 if __name__ == "__main__":
     select_files()
